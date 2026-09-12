6519f4656f552a581fd667813e82a2e8bdd3e986
Philip James Olbedencia <meowbroken@gmail.com>
2026-09-13 00:17:39 +0800

    changes in letter

--- src/App.jsx
+++ src/App.jsx
@@ -188,7 +188,7 @@ function Letter() {
           <p>For your birthday, I also want to wish you the very best in your integrated class and in your future CPA journey. I know how hard you work, and I’m so proud of you. I hope you keep chasing your dreams with the same grace, determination, and heart you give to everyone around you — and I can’t wait to see you pass the CPALE and step into the future you deserve.</p>
           <p>Happy birthday, fa1ry pr1nc355. I hope today feels exactly as special as you make every day feel for me.</p>
         </div>
-        <p className="letter-sign">— by Pejay</p>
+        <p className="letter-sign">— by Pejayyyy</p>
       </div>
     </section>
   )
